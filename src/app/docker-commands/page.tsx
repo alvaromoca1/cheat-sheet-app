57100a416cd0281143e24025d788226e309d6bd7
alvaro morales caballero <alvaromoralescaballero@MacBook-Pro-de-alvaro.local>
2024-11-09 00:04:05 -0500

    incorporando cambio para docker

---
+++ src/app/docker-commands/page.tsx
@@ -0,0 +1,9 @@
+import DockerCommands from '../components/DockerCommands'
+
+export default function DockerCommandsPage() {
+  return (
+    <div className="container mx-auto p-4">
+      <DockerCommands />
+    </div>
+  )
+}
